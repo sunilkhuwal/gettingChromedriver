317540d3ff9783854f25a70296a7228c99efd021
Sunil Kumar <sunil.kumar@metacube.com>
2017-09-19 21:55:57 +0530

    new file

---
+++ GetLatestChromeDriver.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Versions
+CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+chrome_driver_path="/c/chromedriver_win32/"
+chrome_driver_old_versions_path="/c/chromedriver_win32/Old/"
+
+
+if [ ! -d $chrome_driver_path ]; then
+{
+	mkdir $chrome_driver_path
+}
+fi
+if [ ! -d $chrome_driver_old_versions_path ]; then
+{
+	mkdir $chrome_driver_old_versions_path
+}
+fi
+
+#Getting existing driver version
+existing_driver_version=`$chrome_driver_path"chromedriver.exe" -v|awk {'print $2'}`
+echo $existing_driver_version
+
+# Install ChromeDriver.
+mv -f $chrome_driver_path"chromedriver.exe" $chrome_driver_old_versions_path"chromedriver_"$existing_driver_version".exe"
+wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_win32.zip -P $chrome_driver_path/
+unzip $chrome_driver_path"chromedriver_win32.zip" -d $chrome_driver_path
+rm $chrome_driver_path/chromedriver_win32.zip
